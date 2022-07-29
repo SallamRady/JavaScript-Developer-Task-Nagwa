e025d579d469ab96eb6d3a5b1ee5be5758ac577e
SallamRady <sallamrady@gmail.com>
2022-07-29 16:10:07 +0200

    implement footer component

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            Created & designed by <a href='mailto:sallamrady@gmail.com'>Sallam Rady Ramadan</a>
+        </div>
+    );
+};
+
+export default Footer;
