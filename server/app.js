2b1cb0fe1aa6bbb1526a8cff1cb42ffe795005b5
SallamRady <sallamrady@gmail.com>
2022-07-30 14:38:10 +0200

    enable cors

--- server/app.js
+++ server/app.js
@@ -1,25 +1,27 @@
+// imports
 const express = require("express");
 const fs = require("fs");
-const generateList = require('./utils/randomArr');
+const cors = require('cors');
+const randomWorker = require('./utils/randomArr');
+
+
 // declaration
 const port = process.env.PORT || 4000;
 
-// express application
+// express application server & server confogration
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
 /**
  * return a list of 10 objects selected randomly from the "wordsList"
  */
 app.get("/getWords", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Content-Type", "application/json");
-  // Content-Type: application/json
-  let rawdata = fs.readFileSync("./data/TestData.json");
-  let data = JSON.parse(rawdata);
-  let wordsList = data.wordList;
-  let result = generateList(wordsList,10);
+
+  let result = randomWorker.getRandomList(10);
   res.json(result);
   res.end();
+  
 });
 
 app.post("/getRank",(req, res, next)=>{
